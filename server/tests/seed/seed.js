267f39303d7f0a666bf5b82ed3acec7d6f3bdf4c
theinternot <theinternot@gmail.com>
2017-05-15 14:05:40 -0700

    Associate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -20,17 +20,23 @@ const users = [{
 }, {
     _id: userTwoId,
     email: 'dork@dork.com',
-    password: 'userTwoPass'
+    password: 'userTwoPass',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({ _id: userTwoId, access: 'auth' }, 'abc123').toString()
+    }]
 }];
 
 const todos = [{
     _id: new ObjectID(),
-    text: 'first test todo'
+    text: 'first test todo',
+    _creator: userOneId
 }, {
     _id: new ObjectID(),
     text: 'second test todo',
     completed: true,
-    completedAt: 333
+    completedAt: 333,
+    _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
@@ -41,12 +47,12 @@ const populateTodos = (done) => {
 
 
 const populateUsers = (done) => {
-  User.remove({}).then(() => {
-    var userOne = new User(users[0]).save();
-    var userTwo = new User(users[1]).save();
+    User.remove({}).then(() => {
+        var userOne = new User(users[0]).save();
+        var userTwo = new User(users[1]).save();
 
-    return Promise.all([userOne, userTwo])
-  }).then(() => done());
+        return Promise.all([userOne, userTwo])
+    }).then(() => done());
 };
 
 
